55ed7b158e6324bd35ed0e032fae440aa570052e
kiran_waghamode <computers44682@gmail.com>
2024-06-14 03:39:01 -0700

    folder structure created

---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './Navbar.css'
+const Navbar = () => {
+  return (
+    <>
+    <h2>this is navbar</h2>
+    
+    
+    </>
+  )
+}
+
+export default Navbar
---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './Home.css'
+import Navbar from '../../components/navbar/Navbar'
+
+const Home = () => {
+  return (
+    <>
+    <Navbar/>
+    
+    
+    </>
+  )
+}
+
+export default Home
