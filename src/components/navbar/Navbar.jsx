34db11d16e76cfd1c51df06782ee1ee2ccc39815
kiran_waghamode <computers44682@gmail.com>
2024-06-14 04:31:43 -0700

    Navbar and favicon completed

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -3,8 +3,22 @@ import './Navbar.css'
 const Navbar = () => {
   return (
     <>
-    <h2>this is navbar</h2>
-    
+    <div className="nav-main">
+    <div className="nav-left">
+        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTCSPaFQq-Eyoa878pca55a046myfGy9iD2uQ&s" alt="" />
+        <h4>STREAM IT</h4>
+    </div>
+    <div className="nav-center">
+        <input type="text" placeholder='Search'/>
+        <button>Search</button>
+    </div>
+    <div className="nav-right">
+        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTLtgftLHQuwp0CIxSvwqcRFgq3kjfuTA_APg&s" alt="" id='video-icon' />
+
+        <img src="https://m.media-amazon.com/images/M/MV5BMzgwYWNlMDItZDdjOS00NmI3LWJlNGUtNjRkYzYyYjU4OWE5XkEyXkFqcGdeQWRvb2xpbmhk._V1_QL75_UY281_CR0,0,500,281_.jpg" alt="" id='profile-img' />
+
+    </div>
+    </div>
     
     </>
   )
